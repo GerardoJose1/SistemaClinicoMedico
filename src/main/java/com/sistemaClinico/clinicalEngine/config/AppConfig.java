2bba565c6cf2ef20c5f551dfc20c19098ad00815
GerardoJose1 <90854895+GerardoJose1@users.noreply.github.com>
2026-03-24 19:40:48 -0600

    AppConfig para poder conectarse con WordPress
    Signed-off-by: GerardoJose1 <90854895+GerardoJose1@users.noreply.github.com>

--- src/main/java/com/sistemaClinico/clinicalEngine/config/AppConfig.java
+++ src/main/java/com/sistemaClinico/clinicalEngine/config/AppConfig.java
@@ -1,4 +1,40 @@
 package com.sistemaClinico.clinicalEngine.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(5)) // Tiempo máximo para establecer conexión
+                .readTimeout(Duration.ofSeconds(10)) // Tiempo máximo para esperar los datos
+                .errorHandler(new DefaultResponseErrorHandler())
+                .build();
+    }
+
+    @Bean("wordpressWebClient")
+    public WebClient wordpressWebClient(
+            @Value("${wordpress.base-url}") String baseUrl,
+            @Value("${wordpress.username}") String username,
+            @Value("${wordpress.app-password}") String password
+    ) {
+
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeaders( h -> h.setBasicAuth(username, password.replace(" ", "")))
+                .codecs(c -> c.defaultCodecs().maxInMemorySize( 2 * 1024 * 1024))
+                .build();
+
+    }
+
 }
